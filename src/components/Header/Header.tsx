113006bedf17801f00985fabb22ee2612b06c693
Alejandro Yunes <alejandroyunes@outlook.com>
2020-09-05 16:23:03 -0500

    added menu in mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import logo from '../../assets/logo.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import './Header.scss'
-import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
 import { toggleDarkMode } from '../../store/actions/app';
 import { AppStore } from '../../store/appStore';
 
@@ -27,41 +26,65 @@ function Header() {
   const isDarkMode = useSelector((state: AppStore) => state.app.darkMode);
 
   return (
-
     <div className="header">
-      <header className={header}>
+      <header className="header__slide__down">
         <div className="header__logo">
           <img src={logo} />
         </div>
-        <ul className="header__links">
-          <div className="header__toggle__dark_mode">
-            <div className="container">
-              <input type="checkbox" 
-              defaultChecked={isDarkMode}
-              className="chk" id="chk" 
-              onChange={ () => dispatch(toggleDarkMode())}/>
-              <label htmlFor="chk" className="toggle">
-                <span className="star star1"></span>
-                <span className="star star2"></span>
-                <span className="star star3"></span>
-                <span className="star star4"></span>
-                <span className="star star5"></span>
-                <span className="star star6"></span>
-                <span className="handler" >
-                  <span className="crater crater1"></span>
-                  <span className="crater crater2"></span>
-                  <span className="crater crater3"></span>
-                </span>
-              </label>
-            </div>
-          </div>
 
+        <div className="header__toggle__dark_mode_container">
+          <div className="dark-toggle-input">
+            <input
+              type="checkbox"
+              defaultChecked={isDarkMode}
+              className="chk"
+              id="chk"
+              onChange={() => dispatch(toggleDarkMode())}
+            />
+            <label htmlFor="chk" className="toggle">
+              <span className="star star1"></span>
+              <span className="star star2"></span>
+              <span className="star star3"></span>
+              <span className="star star4"></span>
+              <span className="star star5"></span>
+              <span className="star star6"></span>
+              <span className="handler">
+                <span className="crater crater1"></span>
+                <span className="crater crater2"></span>
+                <span className="crater crater3"></span>
+              </span>
+            </label>
+          </div>
+        </div>
+
+        <ul className="header__links">
           <li className="header__link__item">home</li>
           <li className="header__link__item">about</li>
           <li className="header__link__item">blog</li>
           <li className="header__link__item">contact</li>
-          <li className="header__link__item__hamburger"><MenuRoundedIcon style={{ fontSize: 28 }} /></li>
         </ul>
+        <div className="hamburger-menu-mobile">
+          <div id="toggleMenu">
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+              <a href="#">
+                <li>Home</li>
+              </a>
+              <a href="#">
+                <li>About</li>
+              </a>
+              <a href="#">
+                <li>Portfolio</li>
+              </a>
+              <a href="#">
+                <li>Contact</li>
+              </a>
+            </ul>
+          </div>
+        </div>
       </header>
     </div>
   );
