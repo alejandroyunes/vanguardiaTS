172bc4f26706142a0050d80a87b81bc68e19d71e
Alejandro <alejandroyunes@outlook.com>
2020-09-14 00:24:26 -0500

    fixed cards middle animation and added dots

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -14,23 +14,23 @@ export default function Cards() {
 
   const [ fromLeftToRight, setfromLeftToRight] = useState(false);
 
-  const [ fromRightToLeft, setfromRightToLeft] = useState(true);
+  const [ fromRightToLeft, setfromRightToLeft] = useState(false);
   const [ defaultValue, setDefaultValue] = useState(true);
 
-  const changeLeft = ( ) => {
+  const changeLeft = () => {
 
     if (currentCard == 3){
       setfromRightToLeft(false)
       setfromLeftToRight(true)
-     
-      setDefaultValue(true)
+    
+      setDefaultValue(false)
       setCard(currentCard - 1)
-     
+
     }
     if (currentCard == 2){
-      setDefaultValue(true)
+      setDefaultValue(false)
       setCard(currentCard - 1)
-     
+
     }
   }
 
@@ -39,18 +39,20 @@ export default function Cards() {
 
   if (currentCard == 1 ){
     setfromRightToLeft(true)
+    setfromLeftToRight(false)
     setDefaultValue(false)
     setCard(currentCard + 1)
 
+  }else {
+    setDefaultValue(false)
   }
   if (currentCard == 2  ){
-
     setCard(currentCard + 1)
+    
+  }else {
+    setDefaultValue(false)
   }
-
   }
-
-
   return (
     <>
       <Title
@@ -58,10 +60,9 @@ export default function Cards() {
         message="Mobile first world"
       />
       <div className="cards__container section__grid">
-     
         <div className={
             classNames('card__child__hide',
-                     {'card__child__active animated bounceInLeft'  : currentCard == 1}, 
+                     {'card__child__active animated bounceInLeft'  : currentCard == 1},
                      )}>
           <Card
             image={ResponsiveDesignIcon}
@@ -73,10 +74,10 @@ export default function Cards() {
           />
         </div>
 
-{(fromRightToLeft  )&& (
+{(fromRightToLeft ) && (
 
 <div className={
-  classNames('card__child__hide', 
+  classNames('card__child__hide',
   {'card__child__active animated bounceInRight' : currentCard == 2},
   )}>
   <Card
@@ -92,10 +93,10 @@ export default function Cards() {
 )}
 
 
-{(fromLeftToRight && defaultValue)  && (
+{(fromLeftToRight )  && (
 
 <div className={
-  classNames('card__child__hide', 
+  classNames('card__child__hide',
   {'card__child__active animated bounceInLeft' : currentCard == 2},
   )}>
   <Card
@@ -109,8 +110,27 @@ export default function Cards() {
 </div>
 
 )}
+
+{( defaultValue)  && (
+
+<div className={
+  classNames('card__child__hide',
+  {'card__child__active ' : currentCard == 2},
+  )}>
+  <Card
+    image={MobileDesignIcon}
+    title="Responsive Middle"
+    subtitle="Illustration Gallery"
+    message="Leading2 digital agency with solid design and development
+  expertise. We build mobile and web products for startups and
+  design intricate trading platforms."
+  />
+</div>
+
+)}
+
         <div className={
-          classNames('card__child__hide', 
+          classNames('card__child__hide',
           {'card__child__active animated bounceInRight' : currentCard == 3} )}>
           <Card
             image={MobileDesignIcon}
@@ -123,45 +143,57 @@ export default function Cards() {
         </div>
       </div>
       <div className="button__div">
-        <button 
+        <button
                 className={
-                  classNames('arrow left', 
+                  classNames('arrow left',
                   {'arrow-active polyline' : currentCard == 2},
                   {'arrow-active polyline ' : currentCard == 3}
                  )}
                 onClick={changeLeft}>
           <svg width="40px" height="60px" viewBox="0 0 50 80">
-            <polyline 
-            fill="none" 
-            stroke="#AE3C43" 
-            stroke-width="1" 
-            stroke-linecap="round" 
-            stroke-linejoin="round" 
+            <polyline
+            fill="none"
+            stroke="#AE3C43"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
             points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
           </svg>
         </button>
-  
+
         <div className="right__arrow_div">
         <button className={
-                  classNames('arrow right', 
+                  classNames('arrow right',
                   {'arrow-active polyline' : currentCard == 2},
                   {'arrow-active polyline ' : currentCard == 1} )}
-          
+
                 onClick={changeRight}>
           <svg width="40px" height="60px" viewBox="0 0 50 80" >
-            <polyline 
-            fill="none" 
-            stroke="#AE3C43" 
-            stroke-width="1" 
-            stroke-linecap="round" 
-            stroke-linejoin="round" 
+            <polyline
+            fill="none"
+            stroke="#AE3C43"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
             points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
           </svg>
         </button>
         </div>
+        
 
       </div>
-
+      <div className='dots-container'>
+      <div className={
+          classNames('dot',
+          {' dot current' : currentCard == 1} )}></div>
+      <div className={
+          classNames('dot',
+          {' dot current' : currentCard == 2} )}></div>
+          <div className={
+          classNames('dot',
+          {' dot current' : currentCard == 3} )}></div>
+      </div>
+      
     </>
   );
 }
