55f7df7375e6b87beaeeb24fbbce1c5296878a0d
Alejandro Yunes <alejandro.yunes@inxeption.com>
2020-11-08 04:24:21 -0500

    fixed mobile darkmode container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -79,123 +79,8 @@ function Header() {
           <div className="header__logo">
             <img src={logo} />
           </div>
-          <div className="header__toggle__dark_mode_container">
-            <div className="dark-toggle-input">
-              <input
-                type="checkbox"
-                defaultChecked={isDarkMode}
-                className="chk"
-                id="chk"
-                onChange={() => dispatch(toggleDarkMode())}
-              />
-              <label htmlFor="chk" className="toggle">
-                <span className="star star1"></span>
-                <span className="star star2"></span>
-                <span className="star star3"></span>
-                <span className="star star4"></span>
-                <span className="star star5"></span>
-                <span className="star star6"></span>
-                <span className="handler">
-                  <span className="crater crater1"></span>
-                  <span className="crater crater2"></span>
-                  <span className="crater crater3"></span>
-                </span>
-              </label>
-            </div>
-          </div>
-          <ul className="header__links">
-            <Link to={{ pathname: "/" }}>
-              <li
-                id="home-nav"
-                onClick={() => {
-                  setActiveMenu("home-nav");
-                }}
-                className={
-                  activeMenu == "home-nav"
-                    ? "header__link__item active"
-                    : "header__link__item"
-                }
-              >
-                Home
-            </li>
-            </Link>
 
-            <Link to={{ pathname: "/about" }}>
-              <li
-                id="about-nav"
-                onClick={() => {
-                  setActiveMenu("about-nav");
-                }}
-                className={
-                  activeMenu == "about-nav"
-                    ? "header__link__item active"
-                    : "header__link__item"
-                }
-              >
-                About
-            </li>
-            </Link>
-            <Link to={{ pathname: "/services" }}>
-              <li
-                id="services-nav"
-                onClick={() => {
-                  setActiveMenu("services-nav");
-                }}
-                className={
-                  activeMenu == "services-nav"
-                    ? "header__link__item active"
-                    : "header__link__item"
-                }
-              >
-                Services
-            </li>
-            </Link>
-
-
-            <Link to={{ pathname: "/portfolio" }}>
-              <li
-                id="portfolio-nav"
-                onClick={() => {
-                  setActiveMenu("portfolio-nav");
-                }}
-                className={
-                  activeMenu == "portfolio-nav"
-                    ? "header__link__item active"
-                    : "header__link__item"
-                }
-              >
-                Portfolio
-            </li>
-            </Link>
-            <Link to={{ pathname: "/blog" }}>
-              <li
-                id="blog-nav"
-                onClick={() => {
-                  setActiveMenu("blog-nav");
-                }}
-                className={
-                  activeMenu == "blog-nav"
-                    ? "header__link__item active"
-                    : "header__link__item"
-                }
-              >
-                Blog
-            </li>
-            </Link>
-
-            <li
-              id="contact-nav"
-              onClick={() => { scrollBottom() }}
-              className={
-                activeMenu == "contact-nav"
-                  ? "header__link__item active"
-                  : "header__link__item"
-              }
-            >
-              Contact
-            </li>
-
-          </ul>
+          <div className="header__links-wrapper">
           <div className={"hamburger-menu-mobile"}>
             <div id="toggleMenu" onBlur={() => setIsMenuOpen(false)}>
               <input
@@ -229,6 +114,127 @@ function Header() {
               </ul>
             </div>
           </div>
+          <div className="header__toggle__dark_mode_container">
+            <div className="dark-toggle-input">
+              <input
+                type="checkbox"
+                defaultChecked={isDarkMode}
+                className="chk"
+                id="chk"
+                onChange={() => dispatch(toggleDarkMode())}
+              />
+              <label htmlFor="chk" className="toggle">
+                <span className="star star1"></span>
+                <span className="star star2"></span>
+                <span className="star star3"></span>
+                <span className="star star4"></span>
+                <span className="star star5"></span>
+                <span className="star star6"></span>
+                <span className="handler">
+                  <span className="crater crater1"></span>
+                  <span className="crater crater2"></span>
+                  <span className="crater crater3"></span>
+                </span>
+              </label>
+            </div>
+          </div>
+            <ul>
+              <Link to={{ pathname: "/" }}>
+                <li
+                  id="home-nav"
+                  onClick={() => {
+                    setActiveMenu("home-nav");
+                  }}
+                  className={
+                    activeMenu == "home-nav"
+                      ? "header__link__item active"
+                      : "header__link__item"
+                  }
+                >
+                  Home
+            </li>
+              </Link>
+
+              <Link to={{ pathname: "/about" }}>
+                <li
+                  id="about-nav"
+                  onClick={() => {
+                    setActiveMenu("about-nav");
+                  }}
+                  className={
+                    activeMenu == "about-nav"
+                      ? "header__link__item active"
+                      : "header__link__item"
+                  }
+                >
+                  About
+            </li>
+              </Link>
+              <Link to={{ pathname: "/services" }}>
+                <li
+                  id="services-nav"
+                  onClick={() => {
+                    setActiveMenu("services-nav");
+                  }}
+                  className={
+                    activeMenu == "services-nav"
+                      ? "header__link__item active"
+                      : "header__link__item"
+                  }
+                >
+                  Services
+            </li>
+              </Link>
+
+
+              <Link to={{ pathname: "/portfolio" }}>
+                <li
+                  id="portfolio-nav"
+                  onClick={() => {
+                    setActiveMenu("portfolio-nav");
+                  }}
+                  className={
+                    activeMenu == "portfolio-nav"
+                      ? "header__link__item active"
+                      : "header__link__item"
+                  }
+                >
+                  Portfolio
+            </li>
+              </Link>
+              <Link to={{ pathname: "/blog" }}>
+                <li
+                  id="blog-nav"
+                  onClick={() => {
+                    setActiveMenu("blog-nav");
+                  }}
+                  className={
+                    activeMenu == "blog-nav"
+                      ? "header__link__item active"
+                      : "header__link__item"
+                  }
+                >
+                  Blog
+            </li>
+              </Link>
+
+              <li
+                id="contact-nav"
+                onClick={() => { scrollBottom() }}
+                className={
+                  activeMenu == "contact-nav"
+                    ? "header__link__item active"
+                    : "header__link__item"
+                }
+              >
+                Contact
+            </li>
+
+          </ul>
+          </div>
+
+
+         
         </header>
       </div>
 
--- src/containers/Services/Services.tsx
+++ src/containers/Services/Services.tsx
@@ -5,10 +5,8 @@ import aboutBox from ".././../assets/boxeagle.svg";
 import aboutIllustrator from ".././../assets/illustraeagle.svg";
 import Title from "../../components/Title/Title";
 
-import WorkOutlineIcon from "@material-ui/icons/WorkOutline";
 
-import Button from "../../components/Button/Button";
-import Animation from "../../assets/development.png";
+import servicesImg from "../../assets/services.jpg";
 import HtmlSvg from "../../assets/html.svg";
 import CssSvg from "../../assets/css.svg";
 import JsSvg from "../../assets/javascript.svg";
@@ -22,57 +20,10 @@ import ApacheSvg from "../../assets/apache.svg";
 export default function Services() {
   return (
     <>
-      <div className="services-billboard">
-        <div className="services-billboard-child-one">
-          <div>
-            <h1>Vanguardia Digital Development</h1>
-            <h2>
-              Get rid of spam in your application. Use our API to access
-              Akismet’s powerful spam detection. Our developer program provides
-              valuable data and feedback during development.
-            </h2>
-            <div className="buttons">
-              <div className="button-one">
-                <Button
-                  label="view work"
-                  category="primary"
-                  size="sm"
-                  icon={WorkOutlineIcon}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="services-billboard-child-two">
-          <img src={Animation} />
-        </div>
+      <div className="services-page-hero">
+<img src={servicesImg}/>
       </div>
 
-      <div className="services-billboard-mobile">
-        <div className="services-billboard-mobile-child-one">
-          <div>
-            <h1>Vanguardia Digital Development</h1>
-          </div>
-        </div>
-        <div className="services-billboard-mobile-child-two">
-          <img src={Animation} />
-          <h2>
-            Get rid of spam in your application. Use our API to access
-            Akismet’s powerful spam detection. Our developer program provides
-            valuable data and feedback during development.
-            </h2>
-          <div className="buttons">
-            <div className="button-one">
-              <Button
-                label="view work"
-                category="primary"
-                size="sm"
-                icon={WorkOutlineIcon}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
 
       <Title message={"Technologies"} title={""} />
 
@@ -144,8 +95,8 @@ export default function Services() {
         </div>
       </div>
 
-      <div className="about-us-icons-services">
-        <div className="child-one-icons">
+      <div className="services-three-columns">
+        <div className="services-three-columns-child-one">
           <img className="about-logo" src={aboutMarketing} />
           <h4>It can be a fuss to collect images from websites</h4>
           <p>
@@ -153,7 +104,7 @@ export default function Services() {
             procedures and when there are more than one.
           </p>
         </div>
-        <div className="child-two-icons">
+        <div className="services-three-columns-child-two">
           <img className="about-logo" src={aboutBox} />
           <h4>Lack of efficiency to organizing assets</h4>
           <p>
@@ -161,7 +112,7 @@ export default function Services() {
             procedures and when there are more than one.
           </p>
         </div>
-        <div className="child-three-icons">
+        <div className="services-three-columns-child-three">
           <img className="about-logo" src={aboutIllustrator} />
           <h4>Finding assets become mission impossible</h4>
           <p>
