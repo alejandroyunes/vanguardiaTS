f446d378e72c3b0512a3c04666b893a78ecdd4ee
Alejandro Yunes <alejandro.yunes@inxeption.com>
2021-02-21 23:32:03 -0500

    article

--- src/containers/BlogArticle/BlogArticle.tsx
+++ src/containers/BlogArticle/BlogArticle.tsx
@@ -41,7 +41,17 @@ export default function BlogArticle() {
                         <li className="meta-box--item meta-box--share"><section>Share on <a href="#">Twitter</a>,&nbsp;<a href="#">LinkedIn</a></section></li>
                     </ul>
                 </div>
+                <div className="article-description">
+                    <section arial-label="quick summary" className="article__summary">
+                    The present and future of CSS are very bright indeed and if you take a pragmatic, progressive approach to your CSS, then things will continue to get better and better on your projects, too. In this article, we’ll look into masonry layout, :is selector, clamp(), ch and ex units, updated text decoration, and a few other useful CSS properties.
+                    </section>
+                    <section className="article__summary-body">
+                        <p><strong>CSS is great </strong>and getting better all the time. Over recent years, especially, it has evolved really fast, too. Understandably, some of the really handy powers CSS gives you might have slipped you by because of this, so in this article, I’m going to show you some really handy stuff you can do with modern CSS today, and also share some stuff that we can look forward to in the future.</p>
+                        <h2>
 
+                        </h2>
+                    </section>
+                </div>
             </div>
         </>
     )
